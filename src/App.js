8ff3f06191f24450a211dc81a0dba6aa22f2486e
itay3522 <itay5245@gmail.com>
2022-03-10 21:44:08 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 import classes from './App.css';
 
 function App() {
   return (
     <h1 className={classes.name}>Yehudit Englard</h1>
-=======
-import './App.css';
-import Paintings from './components/Paintings';
-
-function App() {
-  return (
-    <Paintings />
->>>>>>> 7caf109d45c51ebcd8745352546d85755b637e88
   );
 }
 
