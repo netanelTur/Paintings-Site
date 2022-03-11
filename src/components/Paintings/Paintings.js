0ffaa846203b265a72b7aeb0eaf99902e2f54d23
itay3522 <itay5245@gmail.com>
2022-03-11 17:15:30 +0200

    fixed overflow-x problem on mobile

--- src/components/Paintings/Paintings.js
+++ src/components/Paintings/Paintings.js
@@ -2,46 +2,100 @@ import "./Paintings.css";
 
 const Paintings = () => {
   return (
-    <div class="row">
+    <div className="container">
+      <div class="row">
         <div class="column">
-          <img src="/assets/paintings/2216.jpg" alt="Snow" className="painting"/>
+          <img
+            src="/assets/paintings/2216.jpg"
+            alt="Snow"
+            className="painting"
+          />
         </div>
         <div class="column">
-          <img src="/assets/paintings/2372.jpg" alt="Forest" className="painting"/>
+          <img
+            src="/assets/paintings/2372.jpg"
+            alt="Forest"
+            className="painting"
+          />
         </div>
         <div className="column">
-          <img src="/assets/paintings/metaneltur.jpg" alt="matanel" className="painting"/>
+          <img
+            src="/assets/paintings/metaneltur.jpg"
+            alt="matanel"
+            className="painting"
+          />
         </div>
         <div className="column">
-          <img src="/assets/paintings/metaneltur.jpg" alt="matanel" className="painting"/>
+          <img
+            src="/assets/paintings/metaneltur.jpg"
+            alt="matanel"
+            className="painting"
+          />
         </div>
         <div className="column">
-          <img src="/assets/paintings/metaneltur.jpg" alt="matanel" className="painting"/>
+          <img
+            src="/assets/paintings/metaneltur.jpg"
+            alt="matanel"
+            className="painting"
+          />
         </div>
         <div className="column">
-          <img src="/assets/paintings/metaneltur.jpg" alt="matanel" className="painting"/>
+          <img
+            src="/assets/paintings/metaneltur.jpg"
+            alt="matanel"
+            className="painting"
+          />
         </div>
         <div className="column">
-          <img src="/assets/paintings/metaneltur.jpg" alt="matanel" className="painting"/>
+          <img
+            src="/assets/paintings/metaneltur.jpg"
+            alt="matanel"
+            className="painting"
+          />
         </div>
         <div className="column">
-          <img src="/assets/paintings/metaneltur.jpg" alt="matanel" className="painting"/>
+          <img
+            src="/assets/paintings/metaneltur.jpg"
+            alt="matanel"
+            className="painting"
+          />
         </div>
         <div className="column">
-          <img src="/assets/paintings/metaneltur.jpg" alt="matanel" className="painting"/>
+          <img
+            src="/assets/paintings/metaneltur.jpg"
+            alt="matanel"
+            className="painting"
+          />
         </div>
         <div className="column">
-          <img src="/assets/paintings/metaneltur.jpg" alt="matanel" className="painting"/>
+          <img
+            src="/assets/paintings/metaneltur.jpg"
+            alt="matanel"
+            className="painting"
+          />
         </div>
         <div className="column">
-          <img src="/assets/paintings/metaneltur.jpg" alt="matanel" className="painting"/>
+          <img
+            src="/assets/paintings/metaneltur.jpg"
+            alt="matanel"
+            className="painting"
+          />
         </div>
         <div className="column">
-          <img src="/assets/paintings/metaneltur.jpg" alt="matanel" className="painting"/>
+          <img
+            src="/assets/paintings/metaneltur.jpg"
+            alt="matanel"
+            className="painting"
+          />
         </div>
         <div className="column">
-          <img src="/assets/paintings/metaneltur.jpg" alt="matanel" className="painting"/>
+          <img
+            src="/assets/paintings/metaneltur.jpg"
+            alt="matanel"
+            className="painting"
+          />
         </div>
+      </div>
     </div>
   );
 };
