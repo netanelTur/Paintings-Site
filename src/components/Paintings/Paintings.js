ac143443953065057499d35914f2c1f73f51d1a2
itay3522 <itay5245@gmail.com>
2022-03-14 17:20:56 +0200

    added about page

--- src/components/Paintings/Paintings.js
+++ src/components/Paintings/Paintings.js
@@ -20,77 +20,126 @@ const Paintings = () => {
         </div>
         <div className="column">
           <img
-            src="/assets/paintings/metaneltur.jpg"
+            src="/assets/paintings/2435.jpg"
             alt="matanel"
             className="painting"
           />
         </div>
         <div className="column">
           <img
-            src="/assets/paintings/metaneltur.jpg"
+            src="/assets/paintings/2458.jpg"
             alt="matanel"
             className="painting"
           />
         </div>
         <div className="column">
           <img
-            src="/assets/paintings/metaneltur.jpg"
+            src="/assets/paintings/2542.jpg"
             alt="matanel"
             className="painting"
           />
         </div>
         <div className="column">
           <img
-            src="/assets/paintings/metaneltur.jpg"
+            src="/assets/paintings/2544.jpg"
             alt="matanel"
             className="painting"
           />
         </div>
         <div className="column">
           <img
-            src="/assets/paintings/metaneltur.jpg"
+            src="/assets/paintings/3065.jpg"
             alt="matanel"
             className="painting"
           />
         </div>
         <div className="column">
           <img
-            src="/assets/paintings/metaneltur.jpg"
+            src="/assets/paintings/3501.jpg"
             alt="matanel"
             className="painting"
           />
         </div>
         <div className="column">
           <img
-            src="/assets/paintings/metaneltur.jpg"
+            src="/assets/paintings/3509.jpg"
             alt="matanel"
             className="painting"
           />
         </div>
         <div className="column">
           <img
-            src="/assets/paintings/metaneltur.jpg"
+            src="/assets/paintings/3575.jpg"
             alt="matanel"
             className="painting"
           />
         </div>
         <div className="column">
           <img
-            src="/assets/paintings/metaneltur.jpg"
+            src="/assets/paintings/3673.jpg"
             alt="matanel"
             className="painting"
           />
         </div>
         <div className="column">
           <img
-            src="/assets/paintings/metaneltur.jpg"
+            src="/assets/paintings/3675.jpg"
             alt="matanel"
             className="painting"
           />
         </div>
         <div className="column">
           <img
-            src="/assets/paintings/metaneltur.jpg"
+            src="/assets/paintings/8774.jpg"
+            alt="matanel"
+            className="painting"
+          />
+        </div>
+        <div className="column">
+          <img
+            src="/assets/paintings/9064.jpg"
+            alt="matanel"
+            className="painting"
+          />
+        </div>
+        <div className="column">
+          <img
+            src="/assets/paintings/9130.jpg"
+            alt="matanel"
+            className="painting"
+          />
+        </div>
+        <div className="column">
+          <img
+            src="/assets/paintings/11910.jpg"
+            alt="matanel"
+            className="painting"
+          />
+        </div>
+        <div className="column">
+          <img
+            src="/assets/paintings/15123.jpg"
+            alt="matanel"
+            className="painting"
+          />
+        </div>
+        <div className="column">
+          <img
+            src="/assets/paintings/16679.jpg"
+            alt="matanel"
+            className="painting"
+          />
+        </div>
+        <div className="column">
+          <img
+            src="/assets/paintings/16719.jpg"
+            alt="matanel"
+            className="painting"
+          />
+        </div>
+        <div className="column">
+          <img
+            src="/assets/paintings/16741.jpg"
             alt="matanel"
             className="painting"
           />
