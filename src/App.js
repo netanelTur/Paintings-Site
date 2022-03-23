7962b5027449f0f06ab9568a8a9c84f514facd3d
Netanel Turgeman <netanel1440@gmail.com>
2022-03-23 20:20:49 +0200

    Fixed about design problem

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ import About from "./components/About/About";
 
 const App = () => {
   return (
-    <div className="container">
+    <div className="main">
       <h1 className="name">Yehudit Englard</h1>
       <div className="router">
         <Router>
           <Navbar />
-            <Routes>
-              <Route path="/" element={<Paintings />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/about" element={<About />} />
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Paintings />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
         </Router>
       </div>
     </div>
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,23 +3,32 @@ import "./About.css";
 const About = () => {
   return (
     <div className="container">
-      <h1 className="title">About Yehudit Englard</h1>
-      <p className="paragraph">
-        Yehudit Englard was born in 1946 in Baghdad, came to Israel five years
-        old and began painting in her forties. <br />
-        Since than, Englard has worked with increasing intensity, and with an
-        uncompromising determination to expand her field of artistic creativity.
-        <br /> Her work is closely identified with the contemporary art form known as
-        intuitive painting. <br /> Yehudit is an autodidactic artist, free of any
-        shackles or preconceptions, <br /> who simply listens to her own internal
-        artistic instincts and gives full rein to the inner strengths waiting to
-        burst free into individual expression. <br /> "Through the process I rely
-        entirely on my feelings, creativity, while constantly giving my all, to
-        abstract human emotions and scenery, with a boundless love for the
-        world.”​ <br /> "Art can be learnt only partially, whereas the artist needs it
-        wholly!" (Goethe) Englard is a member of the Israeli Union of Visual
-        Artists, and the Research of Women Art and Gender Association.
-      </p>
+      <img
+        src="/assets/images/yehudit.png"
+        width="500"
+        height="500"
+        alt="Yeudit Englard"
+        className="profile-img"
+      />
+      <div className="content">
+        <h1 className="title">About Yehudit Englard</h1>
+        <p className="paragraph">
+          Yehudit Englard was born in 1946 in Baghdad, came to Israel five years
+          old and began painting in her forties. <br />
+          Since than, Englard has worked with increasing intensity, and with an
+          uncompromising determination to expand her field of artistic creativity.
+          <br /> Her work is closely identified with the contemporary art form known as
+          intuitive painting. <br /> Yehudit is an autodidactic artist, free of any
+          shackles or preconceptions, <br /> who simply listens to her own internal
+          artistic instincts and gives full rein to the inner strengths waiting to
+          burst free into individual expression. <br /> "Through the process I rely
+          entirely on my feelings, creativity, while constantly giving my all, to
+          abstract human emotions and scenery, with a boundless love for the
+          world.”​ <br /> "Art can be learnt only partially, whereas the artist needs it
+          wholly!" (Goethe) Englard is a member of the Israeli Union of Visual
+          Artists, and the Research of Women Art and Gender Association.
+        </p>
+      </div>
     </div>
   );
 };
