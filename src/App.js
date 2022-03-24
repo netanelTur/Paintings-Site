258f8d9211953c91e369af2edeaefd98b6237c86
Netanel Turgeman <netanel1440@gmail.com>
2022-03-24 19:20:44 +0200

    Aligned text in about

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Paintings from "./components/Paintings/Paintings";
 import Contact from "./components/Contact/Contact";
