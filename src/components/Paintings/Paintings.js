91b098661f79b44d07c2e1a53ca55c3f52118eef
itay3522 <itay5245@gmail.com>
2022-03-31 15:52:09 +0300

    started Modal

--- src/components/Paintings/Paintings.js
+++ src/components/Paintings/Paintings.js
@@ -1,160 +1,182 @@
 import "./Paintings.css";
 import { motion } from "framer-motion";
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
 import { pageVariants, pageTransition } from "./../../animations.js";
 
 const Paintings = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [imgUrl, setImgUrl] = useState('');
+
+  const handleModalOpen = (e) =>  {
+    setIsVisible(true);
+  }
+  const handleModalClose = () => setIsVisible(false);
+
+  //TODO: REMOVE DIVS CLASS AND IMG CLASSNAME AND PUT IT IN CSS
+  
   return (
-    <motion.div
-      className="container"
-      initial="initial"
-      animate="in"
-      exit="out"
-      variants={pageVariants}
-      transition={pageTransition}
-    >
-      <div class="row">
-        <div class="column">
-          <img
-            src="/assets/paintings/2216.jpg"
-            alt="Snow"
-            className="painting"
-          />
+    <div>
+      <Modal show={isVisible} onHide={handleModalClose}>
+        <Modal.Body><img src="/assets/paintings/2216.jpg" /></Modal.Body>
+        <Modal.Footer>
+          <button onClick={handleModalClose}>Close</button>
+        </Modal.Footer>
+      </Modal>
+
+      <motion.div
+        className="container"
+        initial="initial"
+        animate="in"
+        exit="out"
+        variants={pageVariants}
+        transition={pageTransition}
+      >
+        <div class="row">
+          <div class="column">
+            <img
+              src="/assets/paintings/2216.jpg"
+              alt="Snow"
+              className="painting"
+              onClick={handleModalOpen}
+            />
+          </div>
+          <div class="column">
+            <img
+              src="/assets/paintings/2372.jpg"
+              alt="Forest"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/2435.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/2458.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/2542.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/2544.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/3065.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/3501.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/3509.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/3575.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/3673.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/3675.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/8774.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/9064.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/9130.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/11910.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/15123.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/16679.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/16719.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
+          <div className="column">
+            <img
+              src="/assets/paintings/16741.jpg"
+              alt="matanel"
+              className="painting"
+            />
+          </div>
         </div>
-        <div class="column">
-          <img
-            src="/assets/paintings/2372.jpg"
-            alt="Forest"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/2435.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/2458.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/2542.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/2544.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/3065.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/3501.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/3509.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/3575.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/3673.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/3675.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/8774.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/9064.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/9130.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/11910.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/15123.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/16679.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/16719.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-        <div className="column">
-          <img
-            src="/assets/paintings/16741.jpg"
-            alt="matanel"
-            className="painting"
-          />
-        </div>
-      </div>
-    </motion.div>
+      </motion.div>
+    </div>
   );
 };
 
