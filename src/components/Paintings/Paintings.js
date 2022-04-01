706165ec365563214b5f0a0743bea90fbe6b53e8
itay3522 <itay5245@gmail.com>
2022-04-01 18:25:16 +0300

    made it so each click open corresponding image modal

--- src/components/Paintings/Paintings.js
+++ src/components/Paintings/Paintings.js
@@ -10,7 +10,7 @@ const Paintings = () => {
 
   const handleModalOpen = (path) => {
     setIsVisible(true);
-    console.log(path)
+    setImgPath(path);
   };
   const handleModalClose = () => setIsVisible(false);
 
@@ -18,7 +18,7 @@ const Paintings = () => {
     <div>
       <Modal show={isVisible} onHide={handleModalClose} className="modal">
         <Modal.Body>
-          <img src="/assets/paintings/2216.jpg" />
+          <img src={imgPath} />
         </Modal.Body>
         <Modal.Footer>
           <button onClick={handleModalClose}>Close</button>
@@ -37,65 +37,65 @@ const Paintings = () => {
             <img
               src="/assets/paintings/2216.jpg"
               alt="Snow"
-              onClick={handleModalOpen}
+              onClick={() => handleModalOpen("/assets/paintings/2216.jpg")}
             />
           </div>
           <div class="column">
-            <img src="/assets/paintings/2372.jpg" alt="Forest" />
+            <img src="/assets/paintings/2372.jpg" alt="Forest" onClick={() => handleModalOpen("/assets/paintings/2216.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/2435.jpg" alt="matanel" />
+            <img src="/assets/paintings/2435.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/2435.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/2458.jpg" alt="matanel" />
+            <img src="/assets/paintings/2458.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/2458.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/2542.jpg" alt="matanel" />
+            <img src="/assets/paintings/2542.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/2542.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/2544.jpg" alt="matanel" />
+            <img src="/assets/paintings/2544.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/2544.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/3065.jpg" alt="matanel" />
+            <img src="/assets/paintings/3065.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/3065.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/3501.jpg" alt="matanel" />
+            <img src="/assets/paintings/3501.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/3501.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/3509.jpg" alt="matanel" />
+            <img src="/assets/paintings/3509.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/3509.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/3575.jpg" alt="matanel" />
+            <img src="/assets/paintings/3575.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/3575.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/3673.jpg" alt="matanel" />
+            <img src="/assets/paintings/3673.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/3673.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/3675.jpg" alt="matanel" />
+            <img src="/assets/paintings/3675.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/3675.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/8774.jpg" alt="matanel" />
+            <img src="/assets/paintings/8774.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/8774.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/9064.jpg" alt="matanel" />
+            <img src="/assets/paintings/9064.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/9064.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/9130.jpg" alt="matanel" />
+            <img src="/assets/paintings/9130.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/2216.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/11910.jpg" alt="matanel" />
+            <img src="/assets/paintings/11910.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/2216.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/15123.jpg" alt="matanel" />
+            <img src="/assets/paintings/15123.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/2216.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/16679.jpg" alt="matanel" />
+            <img src="/assets/paintings/16679.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/2216.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/16719.jpg" alt="matanel" />
+            <img src="/assets/paintings/16719.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/2216.jpg")}/>
           </div>
           <div className="column">
-            <img src="/assets/paintings/16741.jpg" alt="matanel" />
+            <img src="/assets/paintings/16741.jpg" alt="matanel" onClick={() => handleModalOpen("/assets/paintings/2216.jpg")}/>
           </div>
         </div>
       </motion.div>
