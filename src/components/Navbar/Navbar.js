3ac6c048596f01e119a2d48b965d33125d065bdb
Netanel Turgeman <netanel1440@gmail.com>
2022-04-01 17:59:37 +0300

    Added menu responsively for mobile

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,23 +6,47 @@ import './Navbar.css';
 const Navbar = () => {
   const location = useLocation();
   const [url, setUrl] = useState(null);
+  const [isActive, setActive] = useState("false");
 
   useEffect(() => {
     setUrl(location.pathname);
   }, [location]);
 
+  const toggleMobileMenu = () => {
+    setActive(!isActive);
+  }
+
   return (
-  <div className="navbar">
-    <li>
-      <Link to="/" className={url === "/" ?" active" : ""}>My paintings</Link>
-    </li>
-    <li>
-      <Link to="/contact" className={url === "/contact" ?" active" : ""}>Contact me</Link>
-    </li>
-    <li>
-      <Link to="/about" className={url === "/about" ?" active" : ""}>About me</Link>
-    </li>
-  </div>
+  <>
+    <ul className="navbar">
+        <li>
+          <Link to="/" className={url === "/" ?" active" : ""}>My paintings</Link>
+        </li>
+        <li>
+          <Link to="/contact" className={url === "/contact" ?" active" : ""}>Contact me</Link>
+        </li>
+        <li>
+          <Link to="/about" className={url === "/about" ?" active" : ""}>About me</Link>
+        </li>
+    </ul>
+    <div className={!isActive ? "hamburger-icon open" : "hamburger-icon"} onClick={toggleMobileMenu}>
+      <div className="bar1"></div>
+      <div className="bar2"></div>
+      <div className="bar3"></div>
+
+      <ul className="mobile-menu">
+        <li>
+          <Link to="/" className={url === "/" ?" active" : ""}>My paintings</Link>
+        </li>
+        <li>
+          <Link to="/contact" className={url === "/contact" ?" active" : ""}>Contact me</Link>
+        </li>
+        <li>
+          <Link to="/about" className={url === "/about" ?" active" : ""}>About me</Link>
+        </li>
+      </ul>
+    </div>
+  </>
   );
 }
 export default Navbar;
